2e433a14aa544b14d40b9484f3872b33e696a41c
bmwangangi <trabbart8@gmail.com>
2023-10-11 12:04:23 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,21 @@
 
 /**
  * print_alphabet_x10 - print the alphabet 10 times
+ *
  * Return: void
  */
-
 void print_alphabet_x10(void)
-
 {
 	char ch;
 	int i = 0;
+
 	while (i < 10)
 	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	_putchar(ch);
-	}
-	_putchar('\n');
-	i++;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
 	}
 }
-
