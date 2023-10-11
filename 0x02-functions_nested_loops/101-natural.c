36daf89309ef56614ec4d2a949be280859b31bbe
bmwangangi <trabbart8@gmail.com>
2023-10-11 14:32:39 -0700

    files

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,49 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - check the code.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Description: Computes and prints the sum of multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0
  */
-
-
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
+    int sum = 0;
+    int i;
+
+    for (i = 0; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    char buffer[16];
+    int len = 0;
+
+    while (sum > 0)
+    {
+        buffer[len] = (sum % 10) + '0';
+        sum /= 10;
+        len++;
+    }
+
+    if (len == 0)
+    {
+        buffer[0] = '0';
+        len = 1;
+    }
+
+    for (int j = len - 1; j >= 0; j--)
+    {
+        write(1, &buffer[j], 1);
+    }
+
+    write(1, "\n", 1);
+
+    return (0);
 }
 
