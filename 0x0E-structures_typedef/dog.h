db7f48b28cb7aaf94842a8a5529509f7cade6b8f
bmwangangi <trabbart8@gmail.com>
2023-11-06 11:34:53 -0800

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a structure that holds information about a dog
+ */
+typedef struct dog
+{
+		char *name;
+			float age;
+				char *owner;
+} dog_t;
+
+#endif /* DOG_H */
