34acd5aeca41afa8705b3eda2fe506376009c31b
bmwangangi <trabbart8@gmail.com>
2023-11-16 15:53:36 -0800

    solving 100-first,c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "stdio.h"
 
 /**
- * bmain - function executed before the main function
+ * trab - function executed before the main function
  * Return: void nmber type
  */
-void __attribute__((constructor)) bmain()
+void __attribute__((constructor)) trab()
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!,\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ")
 }
