eceddb25bdbb8c407fa7168a4b5b0e6adbb64b47
bmwangangi <trabbart8@gmail.com>
2023-11-27 06:57:26 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,39 +2,39 @@
 
 /**
  * create_file - Creates a file with specified content.
- * @filename: The name of the file to create.
+ * @filename: The name of the file to be created.
  * @text_content: The NULL-terminated string to write to the file.
  * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, write_status, close_status;
-    ssize_t text_length = 0;
+	int fd, write_status, close_status;
+	ssize_t text_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (file_descriptor == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[text_length] != '\0')
-            text_length++;
+	if (text_content != NULL)
+	{
+		while (text_content[text_length] != '\0')
+			text_length++;
 
-        write_status = write(file_descriptor, text_content, text_length);
-        if (write_status == -1)
-        {
-            close_status = close(file_descriptor);
-            return (-1);
-        }
-    }
+		write_status = write(fd, text_content, text_length);
+		if (write_status == -1)
+		{
+			close_status = close(fd);
+			return (-1);
+		}
+	}
 
-    close_status = close(file_descriptor);
-    if (close_status == -1)
-        return (-1);
+	close_status = close(fd);
+	if (close_status == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
 
