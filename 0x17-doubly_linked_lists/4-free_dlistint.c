178a458a6b9b4c01f523f4fcdb6fb80f7516a845
bmwangangi <trabbart8@gmail.com>
2024-01-11 06:24:50 -0800

    making the files executable

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a dlistint_t list
+ * @head: Pointer to the head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *temp;
+
+	if (head == NULL)
+		return;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+}
+
